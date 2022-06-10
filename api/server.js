d4662e25a6037d6a82b75584d72474f5fcbc820c
Kevin Lai <kevin.lai@gmail.com>
2022-06-10 15:58:57 -0400

    built server and listener

--- api/server.js
+++ api/server.js
@@ -1,9 +1,24 @@
 const express = require('express');
+
 const server = express();
+server.use(express.json());
+
+server.get('*', (req, res) => {
+    res.send(`<h1>Welcome to Projects & Actions</h1>`);
+});
+
+server.use((err, req, res, next) => { 
+    res.status(500).json({
+        message: err.message,
+        stack: err.stack
+    })
+});
+
+module.exports = server;
+
+
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
-
-module.exports = server;
